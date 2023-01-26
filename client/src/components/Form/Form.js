5a5f5a2dfa749de2903af417140863dd4f7cb672
Cosimo <74638028+CosimoV@users.noreply.github.com>
2023-01-26 16:19:45 -0700

    nothing reflects in DB when creating posts

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
+import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
 
 const Form = () => {
 	const [postData, setPostData] = useState({
@@ -10,15 +14,21 @@ const Form = () => {
 		selectedFile: "",
 	});
 	const classes = useStyles();
+	const dispatch = useDispatch();
 
-	const handleSubmit = () => {};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		dispatch(createPost(postData));
+	};
+	const clear = () => {};
 
 	return (
 		<Paper>
 			<form
 				autoComplete="off"
 				noValidate
-				className={classes.form}
+				className={`${classes.root} ${classes.form}`}
 				onSubmit={handleSubmit}
 			>
 				<Typography variant="h6">Creating a Post</Typography>
@@ -58,6 +68,34 @@ const Form = () => {
 					value={postData.tags}
 					onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
 				/>
+				<div className={classes.fileInput}>
+					<FileBase
+						type="file"
+						multiple={false}
+						onDone={({ base64 }) =>
+							setPostData({ ...postData, selectedFile: base64 })
+						}
+					/>
+				</div>
+				<Button
+					className={classes.buttonSubmit}
+					variant="contained"
+					color="primary"
+					size="large"
+					type="submit"
+					fullWidth
+				>
+					Submit
+				</Button>
+				<Button
+					variant="contained"
+					color="secondary"
+					size="small"
+					onClick={clear}
+					fullWidth
+				>
+					Clear
+				</Button>
 			</form>
 		</Paper>
 	);

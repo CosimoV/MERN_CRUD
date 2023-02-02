a095793d9aa70294526259b8131b3cf065191d57
Cosimo <74638028+CosimoV@users.noreply.github.com>
2023-02-02 16:48:53 -0700

    2:07:43

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -8,7 +8,7 @@ export const getPosts = () => async (dispatch) => {
 		dispatch({ type: "FETCH_ALL", payload: data });
 		console.log("get post");
 	} catch (error) {
-		console.log(error.message);
+		console.log(error);
 	}
 };
 
@@ -17,6 +17,36 @@ export const createPost = (post) => async (dispatch) => {
 		const { data } = await api.createPost(post);
 		dispatch({ type: "CREATE", payload: data });
 		console.log("create post");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+	try {
+		const { data } = await api.updatePost(id, post);
+		dispatch({ type: "UPDATE", payload: data });
+		console.log("update post");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const deletePost = (id) => async (dispatch) => {
+	try {
+		await api.deletePost(id);
+		dispatch({ type: "DELETE", payload: id });
+		console.log("delete post");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const likePost = (id) => async (dispatch) => {
+	try {
+		const { data } = await api.likePost(id);
+		dispatch({ type: "UPDATE", payload: data });
+		console.log("like post");
 	} catch (error) {
 		console.log(error);
 	}
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -13,9 +13,11 @@ import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
+import { deletePost, likePost } from "../../../actions/posts";
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
 	const classes = useStyles();
+	const dispatch = useDispatch();
 	return (
 		<Card className={classes.card}>
 			<CardMedia
@@ -30,8 +32,12 @@ const Post = ({ post }) => {
 				</Typography>
 			</div>
 			<div className={classes.overlay2}>
-				<Button style={{ color: "white" }} size="small" onClick={() => {}}>
-					<MoreHorizIcon fontSize="default" />
+				<Button
+					style={{ color: "white" }}
+					size="small"
+					onClick={() => setCurrentId(post._id)}
+				>
+					<MoreHorizIcon fontSize="medium" />
 				</Button>
 			</div>
 			<div className={classes.details}>
@@ -39,18 +45,29 @@ const Post = ({ post }) => {
 					{post.tags.map((tag) => `#${tag} `)}
 				</Typography>
 			</div>
+			<Typography className={classes.title} variant="h5" gutterBottom>
+				{post.title}
+			</Typography>
 			<CardContent>
-				<Typography className="classes.title" variant="h5" gutterBottom>
+				<Typography variant="h5" gutterBottom>
 					{post.message}
 				</Typography>
 			</CardContent>
 			<CardActions className={classes.cardActions}>
-				<Button size="small" color="primary" onClick={() => {}}>
+				<Button
+					size="small"
+					color="primary"
+					onClick={() => dispatch(likePost(post._id))}
+				>
 					<ThumbUpAltIcon fontSize="small" />
 					Like
 					{post.likeCount}
 				</Button>
-				<Button size="small" color="primary" onClick={() => {}}>
+				<Button
+					size="small"
+					color="primary"
+					onClick={() => dispatch(deletePost(post._id))}
+				>
 					<DeleteIcon fontSize="small" />
 					Delete
 				</Button>
